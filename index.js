86407edcea8ca87db1ce40cf9d8f7a1aa43135cb
DenisWAD <denismurray2@gmail.com>
2021-10-05 22:42:50 +0100

    Formatted using Prettier extension

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-
 document.getElementById("button1").addEventListener("click", function () {
-  fetch('https://v2.jokeapi.dev/joke/Any')
+  fetch("https://v2.jokeapi.dev/joke/Any")
     .then((res) => res.json())
     .then((jsonResponse) => displayJoke(jsonResponse))
     .catch(() => displayError());
-
 });
 
 function displayJoke(jokeObject) {
   document.getElementById("setup").innerHTML = jokeObject.setup;
   document.getElementById("delivery").innerHTML = jokeObject.delivery;
   document.getElementById("error").innerHTML = "";
-};
+}
 
 function displayError() {
-  document.getElementById("error").innerHTML = "Oh no's! Something went worng! :O";
-};
+  document.getElementById("error").innerHTML =
+    "Oh no's! Something went worng! :O";
+}
